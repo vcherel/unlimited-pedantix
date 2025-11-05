15495d588fd7a399a7af8207f0a90df9f1fb9017
Valentin CHEREL <valentin.cherel@insa-rennes.fr>
2025-11-05 01:55:59 +0100

    Changed config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 """Parameters"""
 
-NB_ARTICLES = 100                # Number of articles we fetch in total for one game (the best one is kept)
+NB_ARTICLES = 200                # Number of articles we fetch in total for one game (the best one is kept)
 NB_DAYS = 30                     # Number of days we will count the views
 MIN_WORDS = 250                  # Minimum number of words in an article
 SIMILARITY_THRESHOLD = 0.4       # Minimum similarity to show clue
