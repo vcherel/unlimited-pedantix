e053a638c200c34bd074319029383fc4e3f41097
Valentin CHEREL <valentin.cherel@insa-rennes.fr>
2025-11-18 01:13:11 +0100

    Added classifier file

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 """Parameters"""
 
-NB_ARTICLES = 50                # Number of articles we fetch in total for one game (the best one is kept)
+NB_ARTICLES = 100                # Number of articles we fetch in total for one game (the best one is kept)
+NB_ARTICLES_CLASSIFIER = 10      # Number of top articles from which the classifier will choose
 NB_DAYS = 30                     # Number of days we will count the views
 MIN_WORDS = 250                  # Minimum number of words in an article
 SIMILARITY_THRESHOLD = 0.4       # Minimum similarity to show clue
@@ -21,3 +22,6 @@ EXCLUDE_STARTS = [
     "Cet article concerne",
     "N.B."
 ]
+
+if NB_ARTICLES < NB_ARTICLES_CLASSIFIER:
+    raise Exception("NB_ARTICLES should be bigger than NB_ARTICLES_CLASSIFIER")
